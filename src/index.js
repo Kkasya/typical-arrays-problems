adea1b5f33683a6a4bec542cff1e0ddcfef2887b
Kkasya <kasya--k@yandex.by>
2020-09-30 09:08:00 +0300

    feat: add all solves

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array === undefined || array.length === 0 ) return 0;
+    let min = array[0];
+    array.forEach( elem => {
+        if (elem < min) min = elem;
+    });
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array === undefined || array.length === 0 ) return 0;
+    let max = 0;
+    array.forEach( elem => {
+        if (elem > max) max = elem;
+    });
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array === undefined || array.length === 0 ) return 0;
+    let sum = 0;
+    array.forEach( elem => {
+        sum += elem;
+    });
+    return sum/array.length;
 }
